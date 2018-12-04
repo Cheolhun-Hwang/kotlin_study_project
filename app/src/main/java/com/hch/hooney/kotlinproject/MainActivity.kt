797ce23dde82fc4a3973ca50426fe67b3915e080
qewqsa@naver.com <qewqsa@naver.com>
2018-12-04 16:45:12 +0900

    example 1

---
+++ app/src/main/java/com/hch/hooney/kotlinproject/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.hch.hooney.kotlinproject
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.hch.hooney.kotlinproject.t1.TextTypingActivity
+
+class MainActivity : AppCompatActivity() {
+    internal lateinit var exampleOne : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
+    }
+
+    fun init() : Unit {
+        exampleOne = findViewById(R.id.main_exam_one);
+        exampleOne.setOnClickListener{
+            startActivity(Intent(applicationContext, TextTypingActivity::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/com/hch/hooney/kotlinproject/t1/TextTypingActivity.kt
@@ -0,0 +1,57 @@
+package com.hch.hooney.kotlinproject.t1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.hch.hooney.kotlinproject.R
+
+class TextTypingActivity : AppCompatActivity() {
+    val TAG : String = "TextTyping"
+    internal lateinit var sendBTN : Button
+    internal lateinit var showTextView : TextView
+    internal lateinit var edit : EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_text_typing)
+
+        init()
+        setEvent()
+    }
+
+    fun init() : Unit{
+        sendBTN = findViewById(R.id.example_one_send_btn)
+        showTextView = findViewById(R.id.example_one_show_text)
+        edit = findViewById(R.id.example_one_edittext)
+    }
+
+    fun setEvent() : Unit{
+        sendBTN.setOnClickListener(View.OnClickListener {
+            textSendMethod()
+        })
+        edit.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if((event.action == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER) ){
+                textSendMethod()
+                true
+            }else{
+                false
+            }
+        })
+    }
+
+    fun textSendMethod() : Unit{
+        var tempText : String?= null
+        tempText = edit.text.toString()
+        if(tempText.isNullOrEmpty() || tempText.isBlank()){
+            Toast.makeText(applicationContext, "1자 이상 작성해주세요.", Toast.LENGTH_SHORT).show()
+        }else{
+            showTextView.text = tempText
+            edit.text.clear()
+        }
+    }
+}
